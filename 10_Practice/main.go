33b2b5a63730df0796c5bc72e853093f1de95cfd
shubhansu-kr <shubhansu2021@gmail.com>
2022-01-01 16:12:29 +0530

    Practice

---
+++ 10_Practice/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the practice program")
+
+	/*
+		var num1 int
+		var num2 int
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println ("Enter num 1 ")
+		num1, _ := reader.ReadBytes('\n')
+
+		fmt.Println ("Enter num 2 ")
+		num2, _ := reader.ReadBytes('\n')
+	*/
+	
+}
